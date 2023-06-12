8fa66aa608ac1884672206d39855054a41a78271
Sergiy-Chernuha <sergiy.chernuha@gmail.com>
2023-06-12 18:02:19 +0300

    finish work

--- src/main/java/ua/goit/Main.java
+++ src/main/java/ua/goit/Main.java
@@ -1,7 +1,12 @@
-package org.example;
+package ua.goit;
+
+import com.google.gson.Gson;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person i= new Person("Sergiy", "Chernuha");
+       String json = new Gson().toJson(i);
+
+        System.out.println(json);
     }
 }
--- src/main/java/ua/goit/Person.java
+++ src/main/java/ua/goit/Person.java
@@ -1,2 +1,19 @@
-package ua.goit;public class Person {
+package ua.goit;
+
+public class Person {
+    String name;
+    String lastName;
+
+    public Person(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
 }
